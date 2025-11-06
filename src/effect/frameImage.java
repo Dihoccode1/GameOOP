276fa0dabc37c83e073b5d2d5ae645731ccc8b96
Khanhduy <khdidenroiday@gmail.com>
2025-11-07 00:50:12 +0700

    thêm class frameImage

---
+++ src/effect/frameImage.java
@@ -0,0 +1,54 @@
+package effect;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+
+public class frameImage {
+    private String name;
+    private BufferedImage image;
+    
+    //  setter -getter
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+
+    public frameImage(String name , BufferedImage image){
+            setImage(image);
+            setName(name);
+    }
+
+    public frameImage(frameImage frameImages){
+        image= new BufferedImage(frameImages.getImageWidth(),frameImages.getImageHeight(),frameImages.getImage().getType());
+
+        Graphics g = image.getGraphics();
+        g.drawImage(frameImages.getImage(), getImageWidth()/2, getImageHeight(), null);
+    }
+
+    public int getImageWidth(){
+        return image.getWidth();
+    }
+    public int getImageHeight(){
+        return image.getHeight();
+    }
+
+    public void draw(Graphics2D g, int x , int y){
+        g.drawImage(image,x -image.getWidth()/2,y-image.getHeight()/2,null);
+    }
+
+
+}
--- src/main/Interface/GamePanel.java
+++ src/main/Interface/GamePanel.java
@@ -1,25 +1,28 @@
 package main.Interface;
 
 import main.input.keyboardInput;
+
 import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Graphics;
-import main.input.InputManager;
+
 public class GamePanel extends JPanel implements Runnable {
     private keyboardInput keyboard;
     private Thread thread;
     private boolean isRunning = false;
 
+
     public GamePanel() {
         keyboard = new keyboardInput();
         this.addKeyListener(keyboard);
         this.setFocusable(true);
-    }
+}
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setColor(Color.red);
-        g.fillRect(0, 0, getWidth(), getHeight());
+        g.setColor(Color.white);
+     g.fillRect(0,0,GameFrame.SCREEN_WIDTH,GameFrame.SCREEN_HEIGHT);
+       
     }
 
     @Override
@@ -46,8 +49,8 @@ public class GamePanel extends JPanel implements Runnable {
 
             try {
                 Thread.sleep(sleepTime / 1000000); // chuyển ns -> ms
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            } catch (InterruptedException exception) {
+                exception.printStackTrace();
             }
 
             beginTime = System.nanoTime();
